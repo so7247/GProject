a50d28968a1321f11e9755e4785bf3abdec091cf
so7247 <so7247@naver.com>
2017-07-21 14:38:47 +0900

    브런츼 퉤스트ㅡ

--- GProject/src/com/tacademy/data/A.java
+++ GProject/src/com/tacademy/data/A.java
@@ -1,9 +1,19 @@
 package com.tacademy.data;
 
 public class A {
-
+	private int num;
 	public A() {
 		// TODO Auto-generated constructor stub
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	@Override
+	public String toString() {
+		return "A [num=" + num + "]";
 	}
 
 }
