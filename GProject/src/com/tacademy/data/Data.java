ec7458f069d4eb2bc325b7b69c2e644c8840d460
so7247 <so7247@naver.com>
2017-07-21 14:20:35 +0900

    commit!? commit!!

--- GProject/src/com/tacademy/data/Data.java
+++ GProject/src/com/tacademy/data/Data.java
@@ -1,9 +1,23 @@
 package com.tacademy.data;
 
 public class Data {
-
+	private String name;
 	public Data() {
 		// TODO Auto-generated constructor stub
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Data [name=" + name + "]";
+	}
+	
+	
+	
+	
 
 }
